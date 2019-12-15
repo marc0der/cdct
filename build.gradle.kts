864e0c95d48d8e3e46e85b3ea10d68f981ceeda2
Marco Vermeulen <vermeulen.mp@gmail.com>
2019-12-15 12:42:31 +0530

    Remove all code for workshop.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 subprojects {
     apply(plugin = "java-base")
     configure<JavaPluginExtension> {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -8,8 +8,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.slf4j:slf4j-simple:1.7.29")
 
-    implementation("com.github.kittinunf.fuel:fuel:2.2.1")
-    implementation("com.github.kittinunf.fuel:fuel-gson:2.2.1")
-    testImplementation("au.com.dius:pact-jvm-consumer-junit_2.11:3.5.0")
     testImplementation("junit:junit:4.12")
 }
